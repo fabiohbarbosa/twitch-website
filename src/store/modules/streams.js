103b65f12a09ba12c40508c7d8152181c3404948
Fabio H. G. Barbosa <fabio.g.barbosa@mindera.com>
2018-11-21 00:36:20 +0000

    Create stream status

--- src/store/modules/streams.js
+++ src/store/modules/streams.js
@@ -1,3 +1,33 @@
-export default {
+import axios from 'axios';
+import Vue from 'vue';
+import Vuex from 'vuex';
+import VueAxios from 'vue-axios';
 
+Vue.use(Vuex);
+Vue.use(VueAxios, axios);
+
+// initial state
+const state = {
+  all: []
+};
+
+export default {
+  namespaced: true,
+  state,
+  getters: {},
+  actions: {
+    getByGame ({ commit }, game) {
+      axios
+        .get(`/api/streams?game=${game}`)
+        .then(r => r.data)
+        .then(streams => {
+          commit('setStreams', streams);
+        });
+    }
+  },
+  mutations: {
+    setStreams (state, streams) {
+      state.all = streams;
+    }
+  }
 };
