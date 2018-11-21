51fd505857cb9335b4ac07186f46af2e72de5b0c
Fabio H. G. Barbosa <fabio.g.barbosa@mindera.com>
2018-11-21 04:31:06 +0000

    Add footer

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,21 @@
 <template>
   <main>
     <top-menu/>
-
-    <div class="container center-content">
+    <div id="container">
       <router-view />
     </div>
+    <footer-app/>
   </main>
 </template>
 
 <script>
 import TopMenu from 'components/TopMenu';
+import FooterApp from 'components/FooterApp';
 
 export default {
   components: {
-    TopMenu
+    TopMenu,
+    FooterApp
   }
 };
 
---
+++ src/components/FooterApp.vue
@@ -0,0 +1,9 @@
+<template>
+  <div class="footer">
+    <p>Powered By Vue.js / Node.js / Kubernetes </p>
+  </div>
+</template>
+
+<script>
+import './FooterApp.scss';
+</script>
