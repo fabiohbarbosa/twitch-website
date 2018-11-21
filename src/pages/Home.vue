2b542113c48c4ea354cb3a9369269b7f0640baba
Fabio H. G. Barbosa <fabio.g.barbosa@mindera.com>
2018-11-21 04:38:42 +0000

    Lint fix

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -9,17 +9,17 @@
         height="378"
         width="620"/>
 
-      <div class="channel" v-if="stream.channel">
+      <div v-if="stream.channel" class="channel">
         <div class="channel-item">
           <img :src="stream.logo">
         </div>
 
         <div class="channel-info">
           <div class="channel-item">
-            <h1>{{stream.channel}}</h1>
+            <h1>{{ stream.channel }}</h1>
           </div>
           <div class="channel-item">
-            <p>Viewers: {{stream.viewers}}</p>
+            <p>Viewers: {{ stream.viewers }}</p>
           </div>
         </div>
       </div>
